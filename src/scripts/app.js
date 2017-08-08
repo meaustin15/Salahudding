3260f1e3a1918f6a7995de058566364b2b8b5be1
maustin2015 <yebk4lyfe@gmail.com>
2017-08-08 11:01:44 -0500

    cart fix

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -21,11 +21,6 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-contshop.onclick = function() {
-    modal.style.display = "none";
-}
-
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
